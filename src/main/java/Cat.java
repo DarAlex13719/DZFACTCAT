34a78cc83d35887eaa8c081e37f893819fc56a18
Darya Alexeeva <dashuta.alekseeva.01@mail.ru>
2023-03-28 04:57:04 +0300

    Load

---
+++ src/main/java/Cat.java
@@ -0,0 +1,17 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record Cat(String id, String text, String type, String user, String upvotes) {
+
+    public Cat(@JsonProperty("id") String id, @JsonProperty("text") String text, @JsonProperty("type") String type, @JsonProperty("user") String user, @JsonProperty("upvotes") String upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat (" + "id=" + id + ", факт: " + text + ", тип: " + type + ", user: " + user + ", upvotes: " + upvotes + ')';
+    }
+}
